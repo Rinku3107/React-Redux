06b0f461a6c9947c4f52b47b5cd7347759011259
Rinku NImaje <124992262+Rinku3107@users.noreply.github.com>
2024-04-02 10:00:05 +0000

    redux

--- redux/src/Components/Header.js
+++ redux/src/Components/Header.js
@@ -4,22 +4,57 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Badge from '@mui/material/Badge';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 
 function Header() {
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
     return (
         <div>
             <Navbar bg="light" data-bs-theme="light">
                 <Container>
                     <Navbar.Brand href="#home">Vinayak</Navbar.Brand>
                     <Nav className="me-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#home">Home</Nav.Link>
                     </Nav>
-                    <Badge badgeContent={1} color="primary">
-                    <i class="fa-solid fa-cart-shopping"></i>
+                    <Badge badgeContent={1} color="primary" d="demo-positioned-button"
+                        aria-controls={open ? 'demo-positioned-menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={open ? 'true' : undefined}
+                        onClick={handleClick} >
+                        <i class="fa-solid fa-cart-shopping" style={{fontSize:'23px'}}></i>
                     </Badge>
-                    
                 </Container>
+                <Menu
+                    id="demo-positioned-menu"
+                    aria-labelledby="demo-positioned-button"
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                    anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'left',
+                    }}
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'left',
+                    }}
+                >
+                    <div className='card_details d-flex justify-content-center align-items-center'style={{width:'380px',padding:'10px',position:'relative'}}>
+                    <i className="fas fa-close smallclose" style={{position:'absolute', top:2,right:50,fontSize:23,cursor:'pointer'}} onClick={handleClose}></i>
+                    <p>Your Cart is Empty</p>
+                    
+                    </div>
+                </Menu>
             </Navbar>
         </div>
     )
